3ff03458b113f746152920cc6774cc2db8580969
Mi Hwangbo <mihwangbo@Mis-MacBook-Pro.local>
2020-06-04 23:37:21 +0900

    changed api to openweathermap

--- src/Weather/Meteorologist.py
+++ src/Weather/Meteorologist.py
@@ -1,92 +1,19 @@
 import WeatherConfig
-
-from datetime import datetime
-from urllib.request import urlopen
-from urllib.parse import urlencode, unquote, quote_plus
-import urllib
+import requests
 import json
-import os
-import pandas as pd
-
+from datetime import datetime
+import pytz
 
 class Meteorologist:
     def get_weather_data(self):
-        current_time = datetime.now()
-        ultra_srt_ncst = _UltraSrtNcst().get_ultra_srt_ncst(current_time)
-        vilage_fcst = _VilageFcst().get_vilage_fcst(current_time)
-        return [ultra_srt_ncst, vilage_fcst]
+        response = requests.get(WeatherConfig.API_URL)
+        data = json.loads(response.text)
+        return data
 
-# 초단기실황조회
-class _UltraSrtNcst:
-    def get_ultra_srt_ncst(self, current_time):
-        base_date, base_time = self._get_base_time(current_time)
-        try:
-            return _Common().get_fcst_from_api(
-                api_url = WeatherConfig.ULTRA_SRT_URL,
-                base_date = base_date,
-                base_time = base_time
-            )
-        except Exception as e:
-            print(e)
-            return None
-
-    def _get_base_time(self, current_time):
-        date = int(current_time.strftime('%Y%m%d'))
-        hour = int(current_time.strftime('%H'))
-        minute = int(current_time.strftime('%M'))
-        # calculate base time for ultra short cast
-        # report update every 30 min
-        if minute < 40:
-            hour -= 1
-        if hour < 0:
-            hour = 23
-            date -= 1
-        return [str(date), str(hour).zfill(2) + "00"]
-
-# 동네예보조회
-class _VilageFcst:
-    def get_vilage_fcst(self, current_time):
-        base_date, base_time = self._get_base_time(current_time)
-        try:
-            return _Common().get_fcst_from_api(
-                api_url = WeatherConfig.VILAGE_URL,
-                base_date = base_date,
-                base_time = base_time
-            )
-        except Exception as e:
-            print(e)
-            return None
-
-    def _get_base_time(self, current_time):
-        date = int(current_time.strftime('%Y%m%d'))
-        hour = int(current_time.strftime('%H'))
-        minute = int(current_time.strftime('%M'))
-        if minute < 10:
-            hour -= 1
-        if hour < 2:
-            return [str(date - 1), str(23).zfill(2) + "00"]
-        for max_hour, base_hour in WeatherConfig.VILAGE_HOUR_SET.items():
-            if hour < max_hour:
-                return [str(date), str(base_hour).zfill(2) + "00"]
-
-class _Common:
-    def get_fcst_from_api(self, api_url, base_date, base_time):
-        base_params = '?' + urlencode({
-            quote_plus("serviceKey"): WeatherConfig.SERVICE_KEY,
-            quote_plus("numOfRows"): WeatherConfig.NUM_OF_ROWS,
-            quote_plus("pageNo"): WeatherConfig.PAGENO,
-            quote_plus("dataType"): WeatherConfig.DATA_TYPE,
-            quote_plus('nx'): WeatherConfig.NX,
-            quote_plus('ny'): WeatherConfig.NY,
-            quote_plus('base_date'): base_date,
-            quote_plus('base_time'): base_time
-        })
-        try:
-            req = urllib.request.Request(api_url + unquote(base_params))
-            response_body = urlopen(req).read()
-            data = json.loads(response_body)
-            res = data
-        except Exception as e:
-            print(e)
-            res = None
-        return res
+    def clean_weather_data(self, raw_data):
+        print("current: ", float(raw_data["current"]["temp"]) + WeatherConfig.K_C_DIFF)
+        hourly = raw_data["hourly"]
+        for entry in hourly:
+            dt = datetime.fromtimestamp(entry["dt"], pytz.timezone(raw_data['timezone']))
+            temp = float(entry["temp"]) + WeatherConfig.K_C_DIFF
+            print(dt, temp)
--- src/Weather/WeatherConfig.py
+++ src/Weather/WeatherConfig.py
@@ -1,17 +1,18 @@
 import os
 
 
-# for weather api
-SERVICE_KEY = os.environ.get('WEATHER_SERVICE_KEY')
-ULTRA_SRT_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst"
-VILAGE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst"
-DATA_TYPE = "JSON"
-NUM_OF_ROWS = "200"
-PAGENO = "1"
-
-# VilageFcst basetime calc
-VILAGE_HOUR_SET = {2: 23, 5: 1, 8: 5, 11: 8, 14: 11, 17: 14, 20: 17, 23: 20}
-
 # current location
-NX = '62'
-NY = '122'
+LAT = '37.23825'
+LON = '127.17795'
+
+# for weather api
+API_KEY = os.environ.get('WEATHER_API_KEY')
+API_URL = f"https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LON}&appid={API_KEY}"
+
+# calculate temp
+K_C_DIFF = -273.15
+
+# dict for weather data
+CURRENT_W = {"temp": 0, "feels_like": 0, "humidity": 0, "clouds": 0, "wind_speed": 0, "rain": 0, "snow": 0}
+HOURLY_W = {"dt": "", "temp": 0, "feels_like": 0, "humidity": 0, "cloud": 0, "wind_speed": 0, "rain": 0, "snow": 0}
+DAILY_W = {"dt": "", "temp_min": 0, "temp_max": 0, "humidity": 0, "cloud": 0, "wind_speed": 0, "rain": }
