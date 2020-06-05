4260060727d66e23fa34baa7db86726a16399e2e
Mi Hwangbo <mihwangbo@Mis-MacBook-Pro.local>
2020-06-05 14:50:36 +0900

    maybe I do not need to clean data. instead, should put all into the database?

--- src/Weather/Meteorologist.py
+++ src/Weather/Meteorologist.py
@@ -1,4 +1,4 @@
-import WeatherConfig
+from WeatherConfig import *
 import requests
 import json
 from datetime import datetime
@@ -6,14 +6,49 @@ import pytz
 
 class Meteorologist:
     def get_weather_data(self):
-        response = requests.get(WeatherConfig.API_URL)
+        response = requests.get(API_URL)
         data = json.loads(response.text)
         return data
 
     def clean_weather_data(self, raw_data):
-        print("current: ", float(raw_data["current"]["temp"]) + WeatherConfig.K_C_DIFF)
+        print("current: ", float(raw_data["current"]["temp"]) + K_C_DIFF)
+        current = raw_data["current"]
         hourly = raw_data["hourly"]
-        for entry in hourly:
-            dt = datetime.fromtimestamp(entry["dt"], pytz.timezone(raw_data['timezone']))
-            temp = float(entry["temp"]) + WeatherConfig.K_C_DIFF
-            print(dt, temp)
+        daily = raw_data["daily"]
+        
+        self._clean_current_data(current)
+        self._clean_hourly_data(hourly)
+        self._clean_daily_data(daily)
+
+        # weather_dict = {"current": dict, "hourly": dict, "daily": dict}
+        # for entry in hourly:
+        #     dt = datetime.fromtimestamp(entry["dt"], pytz.timezone(raw_data['timezone']))
+        #     temp = float(entry["temp"]) + WeatherConfig.K_C_DIFF
+        #     print(dt, temp)
+
+    def _clean_current_data(self, raw_data):
+        current_dict = dict(CURRENT_W)
+        for key in current_dict:
+            try:
+                current_dict[key] = raw_data[key]
+            except:
+                current_dict[key] = 0
+        print(current_dict)
+
+    def _clean_hourly_data(self, raw_data):
+        hourly_data = []
+        for each_hour in raw_data:
+            hourly_dict = dict(HOURLY_W)
+            for key in hourly_dict:
+                try:
+                    hourly_dict[key] = each_hour[key]
+                except:
+                    hourly_dict[key] = 0
+            hourly_data.append(hourly_dict)
+        print(hourly_data)
+
+
+    def _clean_daily_data(self, raw_data):
+        print(raw_data[0])
+
+Meteorologist().clean_weather_data(Meteorologist().get_weather_data())
--- src/Weather/WeatherConfig.py
+++ src/Weather/WeatherConfig.py
@@ -13,6 +13,6 @@ API_URL = f"https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LON}&
 K_C_DIFF = -273.15
 
 # dict for weather data
-CURRENT_W = {"temp": 0, "feels_like": 0, "humidity": 0, "clouds": 0, "wind_speed": 0, "rain": 0, "snow": 0}
-HOURLY_W = {"dt": "", "temp": 0, "feels_like": 0, "humidity": 0, "cloud": 0, "wind_speed": 0, "rain": 0, "snow": 0}
-DAILY_W = {"dt": "", "temp_min": 0, "temp_max": 0, "humidity": 0, "cloud": 0, "wind_speed": 0, "rain": }
+CURRENT_W = {"temp": 0, "feels_like": 0, "humidity": 0, "clouds": 0, "wind_speed": 0, "weather": 0, "rain": 0, "snow": 0}
+HOURLY_W = {"dt": "", "temp": 0, "feels_like": 0, "humidity": 0, "cloud": 0, "wind_speed": 0, "weather": 0, "rain": 0, "snow": 0}
+DAILY_W = {"dt": "", "temp_min": 0, "temp_max": 0, "humidity": 0, "cloud": 0, "wind_speed": 0, "rain": 0, "snow": 0}
